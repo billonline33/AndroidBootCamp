b8921290d624c2a888674b624f4015a89f30e2f8
billonline33 <billonline33@hotmail.com>
2017-07-12 14:14:32 +1000

    Commit

--- MVP/app/src/main/java/com/example/bhuang/mvp/servicehistory/ServiceHistoryActivity.java
+++ MVP/app/src/main/java/com/example/bhuang/mvp/servicehistory/ServiceHistoryActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
@@ -33,20 +34,39 @@ public class ServiceHistoryActivity extends AppCompatActivity implements Service
 
     private ListView mListView;
     ServiceHistoryAdapter adapter;
+    private RecyclerView mRecycleView;
+    private ServiceHistoryRecycleViewAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_history);
+        mRecycleView=(RecyclerView)findViewById(R.id.recyclevwrServiceHistory);
 
+        /* MVP function
         IServiceHistoryRepository serviceHistoryRepository =new InMemoryServiceHistoryReposity();
         mActionListener=new ServiceHistoryPresenter(serviceHistoryRepository,this);
+        */
+
+        IServiceHistoryRepository serviceHistoryRepository =new InMemoryServiceHistoryReposity();
+        serviceHistoryRepository.getServiceHistories();
+
+        mAdapter=new ServiceHistoryRecycleViewAdapter(serviceHistoryRepository.getServiceHistories());
+        mRecycleView.setAdapter(mAdapter);
 
 
+/*
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                /*
+
+                //Step 1: get Clicked serverhistory
+
+
+                //step 2: call presenter
+                //mActionListener.openServiceHistoryDetails(view,position);
+
+
                 String jobNumber=(String)adapter.getItem(position).getJobNumber();
                 int iD=(int) adapter.getItem(position).getId();
                 ServiceHistory serviceHistory=(ServiceHistory)adapter.getItem(position);
@@ -57,16 +77,17 @@ public class ServiceHistoryActivity extends AppCompatActivity implements Service
                 intent.putExtra("JobNumber",jobNumber);
                 intent.putExtra("ID",iD);
                 startActivityForResult(intent,11);
-                */
+
+
             }
-        });
+        });*/
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mActionListener.loadServiceHistories();
+        //mActionListener.loadServiceHistories();
     }
 
 
@@ -77,12 +98,24 @@ public class ServiceHistoryActivity extends AppCompatActivity implements Service
         ServiceHistoryAdapter adapter;
         adapter=new ServiceHistoryAdapter(this, R.layout.service_history_item_row,serviceHistories);
 
-        mListView=(ListView) findViewById(R.id.lstvwrServiceHistory);
-        mListView.setAdapter(adapter);
+        //mListView=(ListView) findViewById(R.id.lstvwrServiceHistory);
+        //mListView.setAdapter(adapter);
     }
 
     @Override
     public void ShowServiceHistoryDetails() {
 
     }
+
+    public interface ServiceHistoryItemListener {
+
+        void onServiceHistoryItemClicked(ServiceHistory clickedServiceHistory);
+    }
+
+    ServiceHistoryItemListener mItemListener=new ServiceHistoryItemListener() {
+        @Override
+        public void onServiceHistoryItemClicked(ServiceHistory clickedServiceHistory) {
+            //mActionListener.openServiceHistoryDetails(clickedServiceHistory);
+        }
+    };
 }
