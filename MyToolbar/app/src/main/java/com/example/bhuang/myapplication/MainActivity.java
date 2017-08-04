5a81706cab198a89019305e425b85053a393e985
billonline33 <billonline33@hotmail.com>
2017-08-04 15:49:19 +1000

    check in

--- MyToolbar/app/src/main/java/com/example/bhuang/myapplication/MainActivity.java
+++ MyToolbar/app/src/main/java/com/example/bhuang/myapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.bhuang.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -12,15 +18,89 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_1);
         setSupportActionBar(toolbar);
         toolbar.setTitle("123");
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //set Drawerlayout
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.myDrawLayout);
+        mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.open,R.string.close);
+
+        //mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        mDrawerLayout.addDrawerListener(mToggle);
+
+        NavigationView navView=(NavigationView)findViewById(R.id.myNavView);
+
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment frag=null;
+                int intemId=item.getItemId();
+
+                switch (intemId)
+                {
+                    case R.id.navMenu1:
+                        break;
+                    case R.id.navMenu2:
+                        break;
+                    case R.id.navMenu3:
+                        break;
+                    default:
+                        break;
+                }
+
+                if (frag!=null)
+                {
+                   // FragmentTransaction transaction=new getFragmentManager().beginTransaction();
+
+
+                }
+
+
+                return false;
+            }
+        });
+
+
+        /*
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                Toast.makeText(getApplicationContext(),"onDrawerSlide", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                Toast.makeText(getApplicationContext(),"onDrawerOpened", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                Toast.makeText(getApplicationContext(),"onDrawerClosed", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+                Toast.makeText(getApplicationContext(),"onDrawerStateChanged", Toast.LENGTH_LONG).show();
+            }
+        });
+*/
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,6 +131,11 @@ public class MainActivity extends AppCompatActivity {
         }
         if (id == R.id.action_settings2) {
             Toast.makeText(this,"Menu is clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        if (mToggle.onOptionsItemSelected(item))
+        {
             return true;
         }
         return super.onOptionsItemSelected(item);
